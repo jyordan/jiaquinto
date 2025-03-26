84dbb589acf5ac8180bc6f009e41d86bb3744263
Joseph Don Yordan <josephyordan@gmail.com>
2025-03-26 04:17:48 -0600

    Email testing console

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Mail;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('test-code:mail', function () {
+    Mail::raw('This is a test email.', function ($message) {
+        $message->to(env('DEV_EMAIL'))->subject('Test Email');
+    });
+})->purpose('Test email');
