4ec17d3b2a3ad4e650045332d1034f26aa9a9c1d
Joseph Don Yordan <josephyordan@gmail.com>
2025-03-26 08:40:47 -0600

    Test email

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,9 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::command('test-code:mail', function () {
-    Mail::raw('This is a test email.', function ($message) {
-        $message->to(env('DEV_EMAIL'))->subject('Test Email');
+Artisan::command('test-code:mail {email?}', function () {
+    $email = $this->argument('email');
+    Mail::raw('This is a test email.', function ($message) use ($email) {
+        $message->to($email ?: env('DEV_EMAIL'))->subject('Test Email');
     });
 })->purpose('Test email');
