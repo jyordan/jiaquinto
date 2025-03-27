d403ebb9d1247757530a67ff7c603c87742b1da1
Joseph Don Yordan <josephyordan@gmail.com>
2025-03-27 08:14:56 -0600

    Test log

--- app/Http/Controllers/Api/Triggers/VoucherFormController.php
+++ app/Http/Controllers/Api/Triggers/VoucherFormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Triggers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class VoucherFormController extends Controller
 {
@@ -12,7 +13,25 @@ class VoucherFormController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $all = $request->all();
+        $phone = data_get($all, 'customData.phone');
+        $email = data_get($all, 'customData.email');
+        $name = data_get($all, 'customData.name');
+        $workflow = data_get($all, 'workflow.name');
+
+        // Perform the HTTP request using Guzzle
+        $response = Http::asForm()->post(env('GOOGLE_FORM_URL'), [
+            'entry.2135131916' => $name,
+            'entry.715138392' => $phone,
+            'entry.1706893644' => $email,
+            'entry.1016933667' => $workflow,
+            'entry.1297538632' => now()->toDateTimeString(),
+        ]);
+
+        $outcome = $response->successful();
+
         logger($request->all());
+        logger(compact('name', 'phone', 'email', 'name', 'workflow', 'outcome'));
         return response()->json(['outcome' => 'success']);
     }
 }
