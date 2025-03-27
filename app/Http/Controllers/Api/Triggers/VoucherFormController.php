d7514a0e87f0a7d350dba5c716f59fb0b411ea1f
Joseph Don Yordan <josephyordan@gmail.com>
2025-03-27 02:05:30 -0600

    Voucher test log

---
+++ app/Http/Controllers/Api/Triggers/VoucherFormController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\Triggers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VoucherFormController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        logger($request->all());
+        return response()->json(['outcome' => 'success']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,13 @@
 
 use App\Http\Controllers\Api\ClinikoController;
 use App\Http\Controllers\Api\GoHighLevelController;
-use App\Http\Controllers\TempOpportunityController;
+use App\Http\Controllers\Api\Triggers\VoucherFormController;
 use App\Http\Middleware\StaticBasicAuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(StaticBasicAuthMiddleware::class)->group(function () {
-    Route::get('/temp/log/opportunity', TempOpportunityController::class);
+    Route::get('/temp/log/form', VoucherFormController::class);
 
     Route::match(['get', 'post', 'put', 'delete', 'patch',], '/portal/go-high-level/v1/{any}', GoHighLevelController::class)->where('any', '.*');
 
